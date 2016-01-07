a73d8962528b9eb39f6ab44acb624fc9a9425a72
Andy Malik <me@andymalik.com>
2016-01-07 14:58:24 -0800

    Finished TODOs. Got functionality of handlebars working

--- class-04-templates/pair-assignment/carson-andy/starter-code/scripts/article.js
+++ class-04-templates/pair-assignment/carson-andy/starter-code/scripts/article.js
@@ -10,8 +10,13 @@ function Article (opts) {
 }
 
 Article.prototype.toHtml = function() {
-  // TODO: Use handlebars to render your articles.
+  // DONE: Use handlebars to render your articles.
   //       - Get your template from the DOM.
+  var theTemplateScript = $("#address-template").html();
+  var theTemplateFunction = Handlebars.compile(theTemplateScript);
+  var dataSource = this;
+  var theCompiledHtml = theTemplateFunction(dataSource);
+
   //       - Now "compile" your template with Handlebars.
 
   // DONE: If your template will use properties that aren't on the object yet, add them.
@@ -21,7 +26,11 @@ Article.prototype.toHtml = function() {
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
 
-  // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
+  console.log(theCompiledHtml);
+  // DONE: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
+  //$('.content-placeholder').html(theCompiledHtml);
+  //console.log($('.content-placeholder').html(theCompiledHtml));
+  return theCompiledHtml;
 };
 
 rawData.sort(function(a,b) {
@@ -33,5 +42,5 @@ rawData.forEach(function(ele) {
 })
 
 articles.forEach(function(a){
-  $('#articles').append(a.toHtml())
+  $('.content-placeholder').append(a.toHtml())
 });
