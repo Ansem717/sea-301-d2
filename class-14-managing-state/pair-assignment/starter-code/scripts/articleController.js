d8d1234333c77d4c3e740a9d532263837b5a2664
Andy Malik <me@andymalik.com>
2016-01-21 14:37:06 -0800

    Placed comments defining TODOs in articleController.js

--- class-14-managing-state/pair-assignment/starter-code/scripts/articleController.js
+++ class-14-managing-state/pair-assignment/starter-code/scripts/articleController.js
@@ -3,52 +3,57 @@
 
   Article.createTable();  // Ensure the database table is properly initialized
 
-  articlesController.index = function(ctx, next) {
-    articleView.index(ctx.articles);
-  };
+  // These functions are being created, but not called. These functions are called in routes.js by using the page() functions, enabling on click handlers.
+
+  articlesController.index = function(ctx, next) { /***************************************************************/
+    articleView.index(ctx.articles);              /* Create the method which runs the articleView.index on the   */
+  };                                             /*  arbirary 'articles' key of ctx.                            */
+                                                /***************************************************************/
 
   // TODO: Middleware for grabbing one article by ID:
-  articlesController.loadById = function(ctx, next) {
-    var articleData = function(article) {
-      ctx.articles = article;
-      next();
-    };
-
-    Article.findWhere('id', ctx.params.id, articleData);
-  };
+  articlesController.loadById = function(ctx, next) { /*********************************************************/
+    var articleData = function(article) {            /* The method named loadById creates a function named,   */
+      ctx.articles = article;                       /* articleData, which uses a parameter named 'article'   */
+      next();                                      /* When articleData is called, assign the arbitary var   */
+    };                                            /* articles equal to the parameter passed. Then run the  */
+                                                 /* function that is next in line in the page() call.     */
+                                                /*********************************************************/
+    Article.findWhere('id', ctx.params.id, articleData); // Run a SQL query to find all ids equal to the id of the //
+  };                                                    // context. Then pass that to the articleData() function  //
 
   // TODO: Middleware for loading up articles by a certain author:
-  articlesController.loadByAuthor = function(ctx, next) {
-    var authorData = function(articlesByAuthor) {
-      ctx.articles = articlesByAuthor;
-      next();
+  articlesController.loadByAuthor = function(ctx, next) { // Exact same comment as above... 1) Create the method "loadByAuthor"
+    var authorData = function(articlesByAuthor) { // 2) Create but do not call this function "authorData" with one parameter of "articlesByAuthor"
+      ctx.articles = articlesByAuthor; // 3) Set the arbitary "articles" key in history api to the data recieved.
+      next(); // 4) Run the next function in line (as identified in the page() function called).
     };
 
-    Article.findWhere('author', ctx.params.authorName.replace('+', ' '), authorData);
+    Article.findWhere('author', ctx.params.authorName.replace('+', ' '), authorData); // 5) SQL Query to grab data and pass it as a parameter to authorData().
   };
 
   // TODO: Middleware for grabbing all articles with a certain category:
-  articlesController.loadByCategory = function(ctx, next) {
-    var categoryData = function(articlesInCategory) {
-      ctx.articles = articlesInCategory;
-      next();
-    };
+  articlesController.loadByCategory = function(ctx, next) { /*********************************************/
+    var categoryData = function(articlesInCategory) {      /*    ,---.    ,-.     ,-. ,-.   ,-----.     */
+      ctx.articles = articlesInCategory;                  /*     \___    |   |    |  |  |   |_____     */
+      next();                                            /*          |   |---|    |  |  |   |         */
+    };                                                  /*       ----`   |   |    |  |  |   `-----   */
+                                                       /*********************************************/
 
     Article.findWhere('category', ctx.params.categoryName, categoryData);
   };
 
   // TODO: Middleware for grabbing ALL articles:
-  articlesController.loadAll = function(ctx, next) {
-    var articleData = function(allArticles) {
-      ctx.articles = Article.all;
-      next();
+  articlesController.loadAll = function(ctx, next) { // Like before, we are creating a new method named loadAll.
+    var articleData = function(allArticles) { // We are creating a function named articleData
+      ctx.articles = Article.all;  // We're assigning the array of Articles.all to the arbitary variable 'ctx.articles'
+      next(); //Run the next function in line as definited from the page() function.
     };
 
-    if (Article.all.length) {
-      ctx.articles = Article.all;
-      next();
-    } else {
-      Article.fetchAll(articleData);
+    if (Article.all.length) {              /********************************************************************/
+      ctx.articles = Article.all;          /* Here we check if Article.all has length. If it does, then we can */
+      next();                              /* just run a similar function as above. But if it does not, then   */
+    } else {                               /* we need to go grab all the data with the fetchAll method.        */
+      Article.fetchAll(articleData);       /********************************************************************/
     }
   };
 
